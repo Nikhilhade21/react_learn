a47cc6a2ea5920824c78d8d1b5b850fd0b0e5a78
Nikhil Hade <nikhilhade21@gmail.com>
2023-09-30 01:22:33 +0530

    react01

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+// import Chai from 'Chai.jsx'
+
+
+function App() {
+  const user = "chai aur code"
+
+  return (
+    <>
+    <h1>React {user}</h1>
+    <p>testing</p>
+    </>
+  )
+}
+
+export default App
