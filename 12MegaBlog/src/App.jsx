043ddf1bfa72dda028b2a432d38183ba06c986eb
Nikhil Hade <nikhilhade21@gmail.com>
2023-10-14 02:12:10 +0530

    RTE

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -6,13 +6,17 @@ import './App.css'
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-    <>
-    
-    <h1>BLOG APP WITH APPWRITE </h1>
-    
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
