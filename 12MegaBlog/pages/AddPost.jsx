dff837fd91dba1535f4a7676095955c2263082b8
Nikhil Hade <nikhilhade21@gmail.com>
2023-10-15 02:27:14 +0530

    psages

---
+++ 12MegaBlog/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, PostForm } from "../src/components";
+
+function AddPost() {
+    return (
+        <div className="py-8">
+            <Container>
+                <AddPostPostForm />
+            </Container>
+        </div>
+    )
+}
+
+export default AddPost
---
+++ 12MegaBlog/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Signup as SignupComponets } from "../src/components";
+
+function Signup() {
+    <div className="py-8">
+        <SignupComponets />
+    </div>
+}
+
+export default Signup
